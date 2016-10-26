4ec667fc70ebf9aabda00a944ba13eb1d0a3804b
nladuo <kalen25115@gmail.com>
2016-10-26 21:52:37 +0800

    updated style.

--- js/videos.js
+++ js/videos.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+    var modal;
 
 	$.post(OC.generateUrl('/apps/videos/api/getVideos')).success(function (resp) {
 
@@ -13,14 +13,16 @@ $(document).ready(function() {
             item.attr('data-file',video.name);
             item.attr('data-id',video.fileid);
             item.attr('data-path',video.path);
-
             item.addClass('video-item');
 
-            var link = $('<a href="#"></a>');
+            modal = $('<div><img style="width: 60px;margin: 40px auto 0 auto;cursor: pointer" src="../../../core/img/actions/vedio-play.png"/></div>');
+            modal.addClass('modal');
+
+            var link = $('<a href="#" style="margin: 0 auto"></a>');
             link.attr('name', '/index.php/apps/files/download/'+video.path);
             var thumbnail = OC.webroot + '/core/img/filetypes/video.svg';
             link.html('<img id="video-'+video.fileid+'" class="thumb" src="'+thumbnail+'" />');
-            link.click(function(){
+            modal.click(function(){
                 videoViewer.onView(video.name, {
                     dir: video.dir,
                     $file: item
@@ -39,19 +41,45 @@ $(document).ready(function() {
                 }
             });
 
-            // setTimeout(changeTumb(video.fileid), 1000);
-
+            item.append(modal);
 
             item.append(link);
-
             item.append('<p class="videoName">'+video.name+'</p>');
 
             $('#videos').append(item);
+
+
+
+
+        });
+
+
+        //设置遮罩层宽度
+        var _width=$('.thumb').css('width');
+        $('.modal').css("width",_width);
+
+        //鼠标移入
+        $('.video-item a img').mouseenter(function () {
+            $('.modal').hide();
+            $(this).parent().parent().find('.modal').show();
+        });
+
+        //鼠标移出
+        $('.modal').mouseleave(function () {
+            $('.modal').hide();
+        });
+
+
+        $('.modal img').mouseenter(function () {
+            $(this).parent().css("opacity","0.4")
+        });
+        $('.modal img').mouseleave(function () {
+            $(this).parent().css("opacity","0.2")
         });
 		
 	});
-	
-	
+
+
 
 });
 
--- templates/part.content.php
+++ templates/part.content.php
@@ -1,3 +1,3 @@
-<div id='videos' class="hascontrols">
+<div id='videos'>
     
 </div>
